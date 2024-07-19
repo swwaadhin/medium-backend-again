b2345c03ec2e2d95a7844c2370b33012f17640f8
Swadhin <2230216@kiit.ac.in>
2024-07-20 00:35:07 +0530

    medium

---
+++ src/index.ts
@@ -0,0 +1,127 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign,decode,verify } from 'hono/jwt';
+import { userRouter } from './route/user';
+import { blogRouter } from './route/blog';
+
+
+const app = new Hono<{
+  Bindings: {
+  DATABASE_URL: string,
+  JWT_SECRET: string,
+}
+  Variables:{
+      userId:string;
+  }
+
+  
+}>();
+
+app.route("api/v1/user",userRouter);
+app.route("api/v1/blog",blogRouter);
+
+
+// app.post('api/v1/user/signup', async(c) => {
+//   const body =await c.req.json();
+//   const prisma = new PrismaClient({
+//     datasourceUrl: c.env.DATABASE_URL,
+// }).$extends(withAccelerate())
+// try{
+// const user=await prisma.user.create({
+//     data:{
+//     username:body.username,
+//     password:body.password,
+//     name:body.name
+//     }
+// })
+// const jwt=await sign({
+//   id:user.id
+// },c.env.JWT_SECRET); 
+
+//   return c.text(jwt)
+// }
+// catch(e){
+//     c.status(411);
+//     return c.text('User already exit ');
+// }
+// })
+
+
+
+
+// app.post('api/v1/user/signin', async(c) => {
+//   const body =await c.req.json();
+//   const prisma = new PrismaClient({
+//     datasourceUrl: c.env.DATABASE_URL,
+// }).$extends(withAccelerate())
+// try{
+// const user=await prisma.user.findFirst({
+//     where:{
+//     username:body.username,
+//     password:body.password,
+   
+//     }
+// })
+// if(!user){
+//   c.status(403);
+//     return c.json({
+//       message:"Invalid creds"
+//     })
+// }
+
+// const jwt=await sign({
+//   id:user.id
+// },c.env.JWT_SECRET); 
+
+//   return c.text(jwt)
+// }
+// catch(e){
+//     c.status(411);
+//     return c.text('User already exit ');
+// }
+// })
+
+
+
+
+
+
+
+
+// app.post('api/v1/blog', (c) => {
+//   return c.text('Hello Hono!')
+// })
+
+
+
+
+
+
+
+// app.get('api/v1/blog', (c) => {
+//   return c.text('Hello Hono!')
+// })
+
+
+
+
+// app.get('api/v1/blog/blog', (c) => {
+//   return c.text('Hello Hono!')
+// })
+
+
+
+
+
+// app.put('api/v1/blog', (c) => {
+//   return c.text('Hello Hono!')
+// })
+
+
+
+
+
+
+
+export default app
---
+++ src/route/blog.ts
@@ -0,0 +1,151 @@
+
+import {Hono} from "hono";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign,decode,verify } from 'hono/jwt';
+import { signinInput, signupInput, createBlogInput,updateBlogInput } from "../../../common/src/index"
+
+export const blogRouter=new Hono<{
+    Bindings: {
+        DATABASE_URL: string,
+        JWT_SECRET: string,
+      }
+      Variables:{
+        userId:string;
+    }
+}>();
+
+blogRouter.use("/* ",async(c,next)=>{
+    const authHeader=c.req.header("authorization") || "";
+    const user=await verify(authHeader,c.env.JWT_SECRET);
+    if(user){
+        //@ts-ignore
+        c.set("userId",user.id);
+         await next();
+    }
+    else{
+        c.status(403);
+        return c.json({
+            message:"you are logged in"
+        })
+    }
+});
+
+blogRouter.post('/',async (c) => {
+    const body =await c.req.json();
+    const authorId=c.get("userId");
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  const { success } = createBlogInput.safeParse(body);
+	if (!success) {
+		c.status(400);
+		return c.json({ error: "invalid input" });
+	}
+ const blog= await prisma.blog.create({
+    data:{
+        title:body.title,
+        content:body.content,
+        //not working using user input dont know why
+        authorId:1
+        
+    }
+
+  })
+    return c.json({
+        id:blog.id
+    })
+  })
+  
+  
+
+  
+  
+  
+  
+ blogRouter.put('/',async (c) => {
+    const body =await c.req.json();
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const { success } = updateBlogInput.safeParse(body);
+	if (!success) {
+		c.status(400);
+		return c.json({ error: "invalid input" });
+	}
+
+  
+ const blog= await prisma.blog.update({
+    where:{
+        id:body.id
+
+    },
+
+    data:{
+        title:body.title,
+        content:body.content,
+    
+        
+    }
+
+  })
+    return c.json({
+        id:blog.id
+    })
+  })
+  
+  
+  
+  
+  
+  blogRouter.get('/bluk', async(c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+   const blog=await  prisma.blog.findMany();
+
+    return c.json({ 
+        blog
+  })
+  })
+  
+  
+  
+  
+  
+
+
+
+
+  blogRouter.get('/:id', async(c) => {
+
+    const id = c.req.param("id");
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  try{
+ const blog= await prisma.blog.findFirst({
+    where:{
+        id:Number(id)
+
+    },
+
+
+
+  })
+    return c.json({
+    blog
+    })
+}catch(e){
+    c.status(411);
+      return c.json({
+      message:"error has occured"
+      })
+}
+    
+  })
+  
+  
+  
